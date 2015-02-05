7f3b539064193b9dedeeb291d1a2c4a0bebdd292
chi <yjcxy12@gmail.com>
2015-02-05 15:29:02 +0000

    minor

--- flightplan.js
+++ flightplan.js
@@ -29,7 +29,7 @@ plan.local(function(local) {
   local.log('Copy files to remote hosts');
   var filesToCopy = local.exec('git ls-files', {silent: true});
   // rsync files to all the destination's hosts
-  blocal.transfer(filesToCopy, '/tmp/' + tmpDir);
+  local.transfer(filesToCopy, '/tmp/' + tmpDir);
 });
 
 // run commands on remote hosts (destinations)
